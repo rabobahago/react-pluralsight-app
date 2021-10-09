bb34b314e53d7dfe29cf40b9077d2a5bb2a546ab
rabobahago <rabobahago@gmail.com>
2021-10-09 03:17:48 +0100

    invalid and courses pages added

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
-// import PageNotFound from "./PageNotFound";
-// import CoursesPage from "./courses/CoursesPage";
+import PageNotFound from "./PageNotFound";
+import CoursesPage from "./courses/CoursesPage";
 
 function App() {
   return (
@@ -13,8 +13,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
-        {/* <Route path="/courses" component={CoursesPage} />
-        <Route component={PageNotFound} /> */}
+        <Route path="/courses" component={CoursesPage} />
+        <Route component={PageNotFound} />
       </Switch>
     </div>
   );
