7e9444f8d9a836fd3679b36fba380a9be356ee49
rabobahago <rabobahago@gmail.com>
2021-10-09 03:08:00 +0100

    nav link component added

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
-// import Header from "./common/Header";
+import Header from "./common/Header";
 // import PageNotFound from "./PageNotFound";
 // import CoursesPage from "./courses/CoursesPage";
 
 function App() {
   return (
     <div className="container-fluid">
-      {/* <Header /> */}
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
